91414be2578991770be7a8a4662917cc53643d01
rj-90 <ra0633a@american.edu>
2017-09-30 00:44:58 -0400

    updates - json files
    json file integration, done durnig session

--- src/app/components/services/levelDataHandler.service.js
+++ src/app/components/services/levelDataHandler.service.js
@@ -1,370 +1,118 @@
-(function(){
-	'use strict';
+(function() {
+  'use strict';
 
-	angular
-		.module('awkwardAnnie')
-		.service('levelDataHandler', levelDataHandler);
-
-
-	/** @ngInject */
-	function levelDataHandler($log, $stateParams){
-		var service = {
-			choiceScores: {A: 0, B: 3, C: 5},
-			successPaths: ["ACC","CAC","CCA","BBC","BCB","CBB","BCC","CBC","CCB","CCC"],
-			legalLevels: ['negative', 'negative-set1', 'negative-set4', 'positive', 'positive-set1', 'positive-set3'],
-			maxLevel: 7,
-
-			//FULL GAME POS AND NEG
-			levels: {
-				level_1: {  /*~~~~~~~~~~~~~~~~~~~~~~ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['fran_Linear'],
-					startingRoom: "lobby",
-					rooms: {
-						lobby:{
-							fran:{
-								dialogKey: "fran_Linear"
-							}
-						},
-						conferenceRoom:{
-							mike:{}
-						},
-						breakRoom:{
-							charlie:{},
-							luna:{}
-						}
-					}
-				},
-				level_2:{ /*~~~~~~~~~~~~~~~~~~~~~~TWO~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['fran_SmallTk_01'],
-					startingRoom: "conferenceRoom",
-					rooms: {
-						lobby:{
-							fran:{
-								dialogKey: "fran_SmallTk_01"
-							}
-						},
-						conferenceRoom:{
-							mike:{}
-						},
-						breakRoom:{
-							charlie:{},
-							luna:{}
-						}
-					}
-				},
-				level_3:{ /*~~~~~~~~~~~~~~~~~~~~~~THREE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_Linear', 'mike_GR_01'],
-					startingRoom: "lobby",
-					rooms: {
-							conferenceRoom:{
-								mike:{
-									dialogKey: "mike_Linear",
-									secondConvo:{
-										dialogKey: "mike_GR_01"
-									}
-								}
-							},
-							fransOffice:{
-								fran:{}
-							},
-							breakRoom:{
-								charlie:{},
-								luna:{}
-							}
-						}
-				},
-				level_4:{ /*~~~~~~~~~~~~~~~~~~~~~~FOUR~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					//added LL.STwGr.01 : luna_01
-					requiredConversations: ['mike_GR_02', 'fran_GR_01','fran_RQ_01','charlie_01', 'luna_01'],
-					rooms: {
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_GR_02"
-							}
-						},
-						fransOffice:{
-							fran:{
-								dialogKey: "fran_GR_01",
-								secondConvo:{
-									dialogKey: "fran_RQ_01"
-								}
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_01"
-							},
-							luna:{ //added these two
-								dialogKey: "luna_01"
-							}
-						}
-					}
-				},
-				level_5:{ /*~~~~~~~~~~~~~~~~~~~~~~FIVE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					// added CC.RQwGR.01 = ta;led to chatly - //luna01 doesnt play - does it have to do with max levels?
-					requiredConversations: ['mike_SmallTk_01', 'mike_RQ_01', 'fran_GR_02', 'luna_02', 'charlie_RQwGr_02'],
-					rooms:{
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_SmallTk_01",
-								secondConvo:{
-									dialogKey: "mike_RQ_01"
-								}
-							}
-						},
-						fransOffice:{
-							fran:{
-								dialogKey: "fran_GR_02"
-							}
-						},
-						breakRoom:{
-							charlie:{ //added values below
-								dialogKey:"charlie_RQwGr_02"
-							},
-							luna:{
-								dialogKey: "luna_02"
-							}
-						}
-					}
-				},
-				level_6:{ /*~~~~~~~~~~~~~~~~~~~~~~SIX~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_SmallTk_02', 'fran_SmallTk_02', 'charlie_RQ_02', 'Luna_RQ_01'],
-					//charly_RQ_02 --- replaced charly and changed luna to Luna_RQ_01
-					startingRoom: "lobby",
-					rooms:{
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_SmallTk_02"
-							}
-						},
-						fransOffice:{
-							fran:{
-								dialogKey: "fran_SmallTk_02"
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_RQ_02"
-							},
-							luna:{
-								dialogKey: "Luna_RQ_01"
-							}
-						}
-					}
-				},
-				level_7:{ /*~~~~~~~~~~~~~~~~~~~~~~SEVEN~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_RQ_02', 'fran_RQ_02', 'charly_SmallTalk_02', 'Luna_RQ_02'],
-					//added charly_SmallTalk_02 //LL.RQ.02 //Luna_RQ_02
-					startingRoom: "lobby",
-					rooms:{
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_RQ_02"
-							}
-						},
-						breakRoom:{
-							fran:{
-								dialogKey: "fran_RQ_02"
-							},
-							charlie:{
-								dialogKey:"charly_SmallTalk_02"
-							},
-							luna:{
-								dialogKey:"Luna_RQ_02"
-							}
-						}
-					}
-				}
-			},
-
-			setUpForGameType: setUpForGameType,
-			getRoomDialogs: getRoomDialogs
-				};
-
-		var otherLevels = {
-		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AA – Positive – Set 1, A – Negative – Set 1~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-			'positive-set1':{
-				level_1:{ /*~~~~~~~~~~~~~~~~~~~~~~ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_SmallTk_02', 'fran_RQ_02', 'charlie_01', 'Luna_RQ_01'],
-					startingRoom: "lobby",
-					rooms:{
-						lobby:{
-							fran:{
-								dialogKey: "fran_RQ_02"
-							}
-						},
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_SmallTk_02"
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_01"
-							},
-							luna:{
-								dialogKey: "Luna_RQ_01"
-							}
-						}
-					}
-				}
-			},
-
-			'negative-set1':{
-				level_1:{ /*~~~~~~~~~~~~~~~~~~~~~~ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_SmallTk_02', 'fran_RQ_02', 'charlie_01', 'Luna_RQ_01'],
-					startingRoom: "lobby",
-					rooms:{
-						lobby:{
-							fran:{
-								dialogKey: "fran_RQ_02"
-							}
-						},
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_SmallTk_02"
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_01"
-							},
-							luna:{
-								dialogKey: "Luna_RQ_01"
-							}
-						}
-					}
-				}
-			},
+  angular
+    .module('awkwardAnnie')
+    .service('levelDataHandler', levelDataHandler);
 
 
-			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AA – Positive – Set 3~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-			'positive-set3':{
-				level_1:{ /*~~~~~~~~~~~~~~~~~~~~~~ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-					requiredConversations: ['mike_RQ_02', 'fran_SmallTk_02', 'charlie_RQwGr_02', 'luna_01'],
-					startingRoom: "lobby",
-					rooms:{
-						lobby:{
-							fran:{
-								dialogKey: "fran_SmallTk_02"
-							}
-						},
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_RQ_02"
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_RQwGr_02"
-							},
-							luna:{
-								dialogKey: "luna_01"
-							}
-						}
-					}
-				}
-			},
-				//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~AA – Negative – Set 4~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-			'negative-set4':{
-				level_1:{ /*~~~~~~~~~~~~~~~~~~~~~~ONE~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/ //charly_SmallTalk_02charlie_RQ_02
-					requiredConversations: ['mike_RQ_01', 'fran_SmallTk_01', 'charlie_RQ_02', 'luna_02'], //changed luna_02
-					startingRoom: "lobby",
-					rooms:{
-						lobby:{
-							fran:{
-								dialogKey: "fran_SmallTk_01"
-							}
-						},
-						mikesOffice:{
-							mike:{
-								dialogKey: "mike_RQ_01"
-							}
-						},
-						breakRoom:{
-							charlie:{
-								dialogKey: "charlie_RQ_02"
-							},
-							luna:{
-								dialogKey: "luna_02"
-							}
-						}
-					}
-				}
-			}
-		};
-		return service;
+  /** @ngInject */
+  function levelDataHandler($log, $stateParams, $http) {
+    var service = {
+      choiceScores: {
+        A: 0,
+        B: 3,
+        C: 5
+      },
+      successPaths: ["ACC", "CAC", "CCA", "BBC", "BCB", "CBB", "BCC", "CBC", "CCB", "CCC"],
+      legalLevels: ['negative', 'negative-set1', 'negative-set4', 'positive', 'positive-set1', 'positive-set3'],
+      maxLevel: 7,
 
-		function setUpForGameType(gameType){
-			if(gameType == 'negative'){
-				return;
-			}
-			if(gameType == 'positive'){
-				service.successPaths = ["CAA", "ACA", "AAC", "BBA", "BAB", "ABB", "BAA", "ABA", "AAB", "AAA"];
-				service.choiceScores = {A: 5, B: 3, C: 0};
-				return;
-			}
+      //FULL GAME POS AND NEG
+      //dealing with a rpomice - intilization needs to say wait intull promice has been resolved - how httlp works a little bit
+      setUpForGameType: setUpForGameType,
+      getRoomDialogs: getRoomDialogs
+    };
 
-			service.maxLevel = 1;
-			if(gameType.indexOf("positive") === 0){
-				service.successPaths = ["CAA", "ACA", "AAC", "BBA", "BAB", "ABB", "BAA", "ABA", "AAB", "AAA"];
-				service.choiceScores = {A: 5, B: 3, C: 0};
-			}
-			service.levels.level_1= otherLevels[gameType].level_1;
-		}
+    // return $http.get(dialogJsonPaths[dialogKey]).then(function(response){
+    // 	return response.data;
+
+    return service;
+    //get the right file
+    //then do ** that is currenly in the function
+
+
+    // $http.get(levelsPath)
+
+    //evrything here is under the .then of the promise
+    function setUpForGameType(gameType) { //adding return genearates a promise and then we say .then it also genrates a promise - maybe in initlization code - maybe ( not for this one) - go get these configuration files then there are some decg i need to make
+
+      var levelsPath = "assets/LevelJson/levels.json";
+      var otherLevelsPath = "assets/LevelJson/otherlevels.json";
+
+      if (gameType.indexOf("positive") === 0) {
+        service.successPaths = ["CAA", "ACA", "AAC", "BBA", "BAB", "ABB", "BAA", "ABA", "AAB", "AAA"];
+        service.choiceScores = {
+          A: 5,
+          B: 3,
+          C: 0
+        };
+      }
+
+      if (gameType === "negative" || gameType === "positive") {
+        return $http.get(levelsPath).then(function(response) {
+          service.levels = response.data; //parsed
+        })
+      } else {
+        service.maxLevel = 1;
+        return $http.get(otherLevelsPath).then(function(response) {
+          service.levels = response.data[gameType];
+          //service.levels.level_1 - to acsess
+        });
+      }
+    } //end of setUp
+
+
+    //Is there any dialog in this room, if yes, what are they.
+    //Later on check if they've been completed
+    function getRoomDialogs(levelKey, roomKey) {
+      var currentRoomCheck = service.levels[levelKey].rooms[roomKey];
+      var dialogs = [];
+      if (!currentRoomCheck) // not{
+      {
+        return dialogs;
+      }
+
+      angular.forEach(currentRoomCheck, function(characterData, characterName) {
+        if (characterData.dialogKey) {
+          dialogs.push(characterData.dialogKey);
+        }
+        //second convo - come back  if we chaneg this later
+        if (characterData.secondConvo && characterData.secondConvo.dialogKey) {
+          dialogs.push(characterData.secondConvo.dialogKey);
+        }
+      });
+      return dialogs;
+    }
+
+    function getSuccessPaths(dialogKey) {
+      $log.error("are you sure you wanna be using this - should not return paths ");
+      for (var levelKey in service) {
+        if (levelKey.indexOf('level_') === 0) {
+          var levelInfo = service[levelKey];
+          for (var roomKey in levelInfo.rooms) {
+            var roomInfo = levelInfo.rooms[roomKey];
+            for (var charKey in roomInfo.characters) {
+              var charInfo = roomInfo.characters[charKey];
+
+              if (charInfo.dialogKey === dialogKey) {
+                return charInfo.successPaths;
+              }
+
+              if (charInfo.secondConvo && charInfo.secondConvo.dialogKey === dialogKey) {
+                return charInfo.secondConvo.successPaths;
+              }
+
+            }
+          }
+        }
+      }
+
+      // not found!
+      return [];
+    }
 
 
 
-		//Is there any dialog in this room, if yes, what are they.
-		//Later on check if they've been completed
-		function getRoomDialogs(levelKey, roomKey){
-			var currentRoomCheck = service.levels[levelKey].rooms[roomKey];
-			var dialogs = [];
-			if(!currentRoomCheck)// not{
-				{
-					return dialogs;
-				}
-
-			angular.forEach(currentRoomCheck, function(characterData,characterName){
-				if(characterData.dialogKey){
-					dialogs.push(characterData.dialogKey);
-				}
-				//second convo - come back  if we chaneg this later
-				if(characterData.secondConvo && characterData.secondConvo.dialogKey){
-					dialogs.push(characterData.secondConvo.dialogKey);
-				}
-			});
-			return dialogs;
-		}
-
-		function getSuccessPaths(dialogKey) {
-			$log.error("are you sure you wanna be using this - should not return paths ");
-			for (var levelKey in service) {
-				if (levelKey.indexOf('level_')===0) {
-					var levelInfo = service[levelKey];
-					for (var roomKey in levelInfo.rooms) {
-						var roomInfo = levelInfo.rooms[roomKey];
-						for (var charKey in roomInfo.characters) {
-							var charInfo = roomInfo.characters[charKey];
-
-							if (charInfo.dialogKey===dialogKey) {
-								return charInfo.successPaths;
-							}
-
-							if (charInfo.secondConvo && charInfo.secondConvo.dialogKey===dialogKey) {
-								return charInfo.secondConvo.successPaths;
-							}
-
-						}
-					}
-				}
-			}
-
-			// not found!
-			return [];
-		}
-
-
-
-	}//end of controller
+  } //end of controller
 })();
